0334d9caaa4341291daf1364a724ca78a53ec624
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-10-07 15:40:01 -0400

    LP

---
+++ models/nouvelle.js
@@ -0,0 +1,20 @@
+const Model = require('./model');
+module.exports =
+    class Nouvelle extends Model {
+        constructor(title ,category , texte , imageUrl , date) {
+            super();
+            this.Title = title !== undefined ? title : "";
+            this.Category = category !== undefined ? category : "";
+            this.Texte = texte!== undefined ? category : "";
+            this.ImageUrl = imageUrl !== undefined ? category : "";
+            this.Date = date !== undefined ? category : "";
+            
+            this.setKey("Url");
+            this.addValidator('Title', 'string');
+            this.addValidator('Category', 'string');
+            this.addValidator('Texte', 'string');
+            this.addValidator('Category', 'string');
+            this.addValidator('ImageUrl', 'url');
+            this.addValidator('Date', 'date');
+        }
+    }
